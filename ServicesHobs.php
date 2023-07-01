8ff4e0af23ea7168c776ae5587d8ab79a5302f9e
unknown <deolhonofonte@gmail.com>
2023-07-01 19:22:20 -0300

    Criando lista parte 2

--- ServicesHobs.php
+++ ServicesHobs.php
@@ -1,10 +1,35 @@
 <?php
 
 
-$content = file_get_contents("https://semprebem.paguemenos.com.br/posts/mente-e-comportamento/lista-de-hobbies",true);
+
+// Lê um arquivo em um array.  Nesse exemplo nós obteremos o código fonte de
+// uma URL via HTTP
+$lines = file ('https://www.maioresemelhores.com/hobbies-para-curtir-tempo-livre/');
+
+// Percorre o array, mostrando o fonte HTML com numeração de linhas.
+foreach ($lines as $line_num => $line) {
+  //  echo "Linha #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br>\n";
+    echo  " " . ($line) . "<br>\n";
+}
+
+// Outro exemplo, onde obtemos a página web inteira como uma string. Veja também file_get_contents().
+//$html = implode ('', file ('https://www.maioresemelhores.com/hobbies-para-curtir-tempo-livre/'));
+
+// Usando o parâmetro de flags opcionais disponíveis desde o PHP 5
+//$trimmed = file('somefile.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 
-print_r($content);
+
+
+
+
+
+
+
+//$content = file_get_contents("https://semprebem.paguemenos.com.br/posts/mente-e-comportamento/lista-de-hobbies",true);
+
+
+//print_r($content);
 /*
 
 $re = '/(<h3>.+\s<\/h3>)/m';
