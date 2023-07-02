c92019b19a5c5ff858bd6442ce6a12786f5f58d0
unknown <deolhonofonte@gmail.com>
2023-07-02 12:31:04 -0300

    adicao no  git ignore

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
 <?php
-require_once './../vendor/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader('../view');
-$twig = new \Twig\Environment($loader, []);
-
-
-
-
-
-
-echo $twig->render('index.html', ['name' => 'Fabien','numeros' => [1,2,58,7,4]]);
+require_once("twigInicializacao.php");
+echo $twig->render('cadastro/pessoa/index.html', ['name' => 'Fabien','numeros' => [1,2,58,7,4]]);
